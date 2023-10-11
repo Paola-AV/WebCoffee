668570da41d758422638f4d5b71446d4723dd4f1
Paola Arrieta <vargasp437@gmail.com>
2023-10-11 13:13:03 -0600

    cambios

--- src/components/HistoriaSc.js
+++ src/components/HistoriaSc.js
@@ -13,15 +13,15 @@ export function HistoriaSc() {
         AOS.refresh();
     }, []);
     return (
-        <section class="w-full bg-[#f0bc84] pb-10 grid">
+        <section class="w-full bg-[#f0bc84] pb-10 grid ">
             <div class="w-[90%] flex flex-row justify-end pt-[7rem]">
                 <h1 class="text-4xl text-[#424530] font-lucida pb-2 mr-1 sm: text-5xl">Nuestra</h1>
                 <h1 class="text-4xl text-[#636845] font-lucida pb-2 ml-1 sm: text-5xl">Historia</h1>
             </div>
 
-            <div class="flex flex-row justify-center font-lucida mt-10 p-3">
+            <div class="flex flex-row justify-center justify-self-center font-lucida mt-10 p-3 mb-10 xl:w-[80%]">
                 <div class="hidden lg:flex z-[1000]">
-                    <img src={Nbarista} class="rounded-full border-8 border-[#424530]"/>
+                    <img src={Nbarista} class="rounded-full border-8 border-[#424530]" />
                 </div>
                 <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
                     <div class="bg-[#FFEFCD] shadow-xl w-full p-2 rounded-br-[4rem] pl-[5rem] translate-x-[-4rem]">
@@ -35,8 +35,8 @@ export function HistoriaSc() {
 
             </div>
 
-            <div class="flex flex-row justify-center font-lucida mt-10 p-3">
-                
+            <div class="flex flex-row justify-center font-lucida mt-10 p-3 mb-10 justify-self-center xl:w-[80%]">
+
                 <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
                     <div class="bg-[#FFEFCD] shadow-xl w-full p-2 rounded-tl-[4rem] pr-[3rem] translate-x-[2rem]">
                         <h2 class="font-semibold pt-4 pl-1">Lorem Ipsum</h2>
@@ -48,14 +48,38 @@ export function HistoriaSc() {
                 </div>
 
                 <div class="hidden lg:flex z-[1000]">
-                    <img src={NchemexPouring} class="rounded-full border-8 border-[#424530]"/>
+                    <img src={NgranosTostadora} class="rounded-full border-8 border-[#424530]" />
                 </div>
 
             </div>
 
-            <div class="flex flex-row justify-center font-lucida mt-10 p-3">
+            <div class="flex flex-row justify-center font-lucida mt-10 p-3 mb-10 justify-self-center xl:w-[80%]">
+                <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
+                    <div class="bg-[#FFEFCD] shadow-xl w-full  rounded-bl-[4rem] pr-[10rem] pl-1">
+                        <h2 class="font-semibold pt-4 pl-[1rem]">Lorem Ipsum</h2>
+                        <p class="pb-[1rem] pl-[1rem]">
+                            Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="hidden lg:grid z-[1000]">
+                    <img src={NchemexPouring}  class="rounded-full border-8 border-[#424530] justify-self-center w-[15rem] absolute self-center" />
+                </div>
+
+                <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
+                    <div class="bg-[#FFEFCD] shadow-xl w-full p-2 rounded-tr-[4rem] pl-[10rem] translate-x-[-2rem]">
+                        <h2 class="font-semibold pt-4 pl-1">Lorem Ipsum</h2>
+                        <p >
+                            Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-row justify-center font-lucida mt-10 p-3 mb-10 justify-self-center xl:w-[80%]">
                 <div class="hidden lg:flex z-[1000]">
-                    <img src={NtoastGaleria} class="rounded-full border-8 border-[#424530]"/>
+                    <img src={NtoastGaleria} class="rounded-full border-8 border-[#424530]" />
                 </div>
                 <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
                     <div class="bg-[#FFEFCD] shadow-xl w-full p-2 rounded-br-[4rem] pl-[5rem] translate-x-[-4rem]">
@@ -69,8 +93,8 @@ export function HistoriaSc() {
 
             </div>
 
-            <div class="flex flex-row justify-center font-lucida mt-10 p-3">
-                
+            <div class="flex flex-row justify-center font-lucida mt-10 p-3 mb-10 justify-self-center xl:w-[80%]">
+
                 <div data-aos="fade-up" class="w-[85%] flex flex-col justify-center">
                     <div class="bg-[#FFEFCD] shadow-xl w-full p-2 rounded-tl-[4rem] pr-[3rem] translate-x-[2rem]">
                         <h2 class="font-semibold pt-4 pl-1">Lorem Ipsum</h2>
@@ -82,7 +106,7 @@ export function HistoriaSc() {
                 </div>
 
                 <div class="hidden lg:flex z-[1000]">
-                    <img src={Nchemex} class="rounded-full border-8 border-[#424530]"/>
+                    <img src={Nchemex} class="rounded-full border-8 border-[#424530]" />
                 </div>
 
             </div>
