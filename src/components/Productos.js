261ffd5e9efe426ae9fb5d79d58d921934cb3f95
Paola Arrieta <vargasp437@gmail.com>
2023-10-16 18:07:26 -0600

    cambio

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -3,7 +3,7 @@ import Paquete from '../images/Paquete.png'
 export function Productos() {
     return (
         <section class="grid pt-[10rem]  bg-[#FFEFCD]">
-            <div class="w-[90%] flex flex-row justify-end pt-4">
+            <div class="w-[90%] flex flex-row justify-end pt-4 mb-[5rem]">
                 <h1 class="text-4xl text-[#424530] font-lucida pb-2 mr-1 font-bold sm:text-5xl 2xl:text-6xl">Nuestros Productos</h1>
             </div>
 
@@ -12,8 +12,8 @@ export function Productos() {
                 <div class="bg-transparent md:bg-[#E09132] rounded-full w-[15rem] h-[15rem] mb-[5rem] lg:w-[15rem] h-[15rem] xl:w-[15rem] h-[15rem] 2xl:h-[19rem] 2xl:w-[19rem]">
                     <div class="flex flex-row absolute items-center">
                         <img class="w-[10.5rem] h-auto translate-x-[2.5rem] drop-shadow-xl 2xl:w-[13rem]" src={Paquete} />
-                        <div class="rounded-r-md bg-transparent shadow-none flex flex-col sm:bg-[#FFEFCD] h-[12rem] md:h-[9rem] w-[60%] justify-center items-start rounded-r-full shadow-lg xl:w-[50%] 2xl:h-[8rem] 2xl:w-[45%]">
-                            <p class="bg-[#424530] pl-10 w-[90%] pb-1 pr-5 rounded-r-full text-[#E09132] font-semibold font-lucida sm:h-[10rem] pt-5"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                        <div class="bg-transparent h-[7rem] w-[50%] flex flex-col justify-center items-start rounded-r-full lg:hadow-lg lg:lg:bg-[#FFEFCD]  lg:max-xl:h-[10rem] 2xl:h-[8rem]">
+                            <p class="bg-[#424530] rounded-lg pl-10 w-[90%] pb-1 pr-5 text-[#E09132] font-semibold font-lucida sm:rounded-r-full sm:max-md:h-[10rem] pt-5 lg:max-xl:h-[70%] lg:max-xl:pt-1"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
                         </div>
                     </div>
 
@@ -23,8 +23,8 @@ export function Productos() {
                 <div class="bg-transparent md:bg-[#E09132] rounded-full w-[15rem] h-[15rem] mb-[5rem] lg:w-[15rem] h-[15rem] xl:w-[15rem] h-[15rem] 2xl:h-[19rem] 2xl:w-[19rem]">
                     <div class="flex flex-row absolute items-center">
                         <img class="w-[10.5rem] h-auto translate-x-[2.5rem] drop-shadow-xl 2xl:w-[13rem]" src={Paquete} />
-                        <div class="bg-[#424530] flex flex-col sm:h-[12rem] md:h-[9rem] w-[60%] justify-center items-start rounded-r-full shadow-lg xl:w-[50%] 2xl:h-[8rem] 2xl:w-[45%]">
-                            <p class="bg-[#FFEFCD] pl-10 w-[90%] pb-1 pr-5 rounded-r-full text-[#E09132] font-semibold font-lucida sm:h-[10rem] pt-5"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                        <div class="bg-transparent h-[7rem] w-[50%] flex flex-col justify-center items-start rounded-r-full lg:shadow-lg lg:bg-[#424530] lg:max-xl:h-[10rem] 2xl:h-[8rem]">
+                            <p class="bg-[#FFEFCD] rounded-lg pl-10 w-[90%] pb-1 pr-5 text-[#E09132] font-semibold font-lucida sm:rounded-r-full sm:max-md:h-[10rem] pt-5 lg:max-xl:h-[70%] lg:max-xl:pt-1"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
                         </div>
                     </div>
 
@@ -34,8 +34,8 @@ export function Productos() {
                 <div class="bg-transparent md:bg-[#E09132] rounded-full w-[15rem] h-[15rem] mb-[5rem] lg:w-[15rem] h-[15rem] xl:w-[15rem] h-[15rem] 2xl:h-[19rem] 2xl:w-[19rem]">
                     <div class="flex flex-row absolute items-center">
                         <img class="w-[10.5rem] h-auto translate-x-[2.5rem] drop-shadow-xl 2xl:w-[13rem]" src={Paquete} />
-                        <div class="bg-[#FFEFCD] flex flex-col sm:h-[12rem] md:h-[9rem] w-[60%] justify-center items-start rounded-r-full shadow-lg xl:w-[50%] 2xl:h-[8rem] 2xl:w-[45%]">
-                            <p class="bg-[#424530] pl-10 w-[90%] pb-1 pr-5 rounded-r-full text-[#E09132] font-semibold font-lucida sm:h-[10rem] pt-5"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                        <div class="bg-transparent h-[7rem] w-[50%] flex flex-col justify-center items-start rounded-r-full lg:hadow-lg lg:lg:bg-[#FFEFCD]  lg:max-xl:h-[10rem] 2xl:h-[8rem]">
+                            <p class="bg-[#424530] rounded-lg pl-10 w-[90%] pb-1 pr-5 text-[#E09132] font-semibold font-lucida sm:rounded-r-full sm:max-md:h-[10rem] pt-5 lg:max-xl:h-[70%] lg:max-xl:pt-1"> Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
                         </div>
                     </div>
 
--- src/components/Talleres.js
+++ src/components/Talleres.js
@@ -9,32 +9,32 @@ export function Talleres() {
 
             <div class="justify-self-center w-[60%] mt-[5rem] 2xl:w-[70%]">
 
-                <div class="bg-[#E09132] rounded-full w-[20rem] h-[20rem] mb-[10rem] grid items-center 2xl:w-[25rem] h-[25rem]">
-                    <div class="bg-[#424530] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around xl:w-[40%] 2xl:h-[20%] w-[35%] left-[23rem]">
-                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] xl:ml-[2rem] 2xl:w-[12rem]"></img>
+                <div class="bg-[#E09132] rounded-full w-[20rem] h-[20rem] mb-[10rem] grid items-center 2xl:w-[25rem] 2xl:h-[25rem]">
+                    <div class="bg-[#424530] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around lg:max-xl:left-[17rem] h-[25%] 2xl:h-[20%] 2xl:w-[35%] left-[23rem]">
+                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] lg:max-xl:ml-[1rem] xl:ml-[2rem] xl:max-2xl:w-[11.5rem] 2xl:w-[12rem]"></img>
                         <div class="flex flex-col bg-[#FFEFCD] font-lucida rounded-lg shadow-xl w-[40%] absolute justify-self-end self-center mr-[3rem] p-4 xl:w-[45%] mr-[2rem] 2xl:w-[40%]">
-                            <h1 class="text-2xl font-bold">Lorem ipsum</h1>
-                            <p class="text-xl">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                            <h1 class="text-2xl font-bold lg:max-xl:text-xl">Lorem ipsum</h1>
+                            <p class="text-xl lg:max-xl:text-base">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="bg-[#424530] rounded-full w-[20rem] h-[20rem] mb-[10rem] grid items-center 2xl:w-[25rem] h-[25rem]">
-                    <div class=" bg-[#FFEFCD] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around xl:w-[40%] 2xl:h-[20%] w-[35%] left-[23rem]">
-                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] xl:ml-[2rem] 2xl:w-[12rem]"></img>
+                <div class="bg-[#424530] rounded-full w-[20rem] h-[20rem] mb-[10rem] grid items-center 2xl:w-[25rem] 2xl:h-[25rem]">
+                    <div class=" bg-[#FFEFCD] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around lg:max-xl:left-[17rem] h-[25%] 2xl:h-[20%] 2xl:w-[35%] left-[23rem]">
+                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] lg:max-xl:ml-[1rem] xl:ml-[2rem] xl:max-2xl:w-[11.5rem] 2xl:w-[12rem]"></img>
                         <div class="flex flex-col bg-[#E09132] font-lucida rounded-lg shadow-xl w-[40%] absolute justify-self-end self-center mr-[3rem] p-4 xl:w-[45%] mr-[2rem] 2xl:w-[40%]">
-                            <h1 class="text-2xl font-bold">Lorem ipsum</h1>
-                            <p class="text-xl">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                            <h1 class="text-2xl font-bold lg:max-xl:text-xl">Lorem ipsum</h1>
+                            <p class="text-xl lg:max-xl:text-base">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="bg-[#E09132] rounded-full w-[20rem] h-[20rem] mb-[5rem] grid items-center 2xl:w-[25rem] h-[25rem]">
-                    <div class="bg-[#424530] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around xl:w-[40%] 2xl:h-[20%] w-[35%] left-[23rem]">
-                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] xl:ml-[2rem] 2xl:w-[12rem]"></img>
+                <div class="bg-[#E09132] rounded-full w-[20rem] h-[20rem] mb-[5rem] grid items-center 2xl:w-[25rem] 2xl:h-[25rem]">
+                    <div class="bg-[#424530] w-[45%] absolute left-[20rem] h-[30%] grid grid-cols-1 justify-around lg:max-xl:left-[17rem] h-[25%] 2xl:h-[20%] 2xl:w-[35%] left-[23rem]">
+                        <img src={TallerT} class="w-[10rem] h-auto absolute self-center ml-[3rem] lg:max-xl:ml-[1rem] xl:ml-[2rem] xl:max-2xl:w-[11.5rem] 2xl:w-[12rem]"></img>
                         <div class="flex flex-col bg-[#FFEFCD] font-lucida rounded-lg shadow-xl w-[40%] absolute justify-self-end self-center mr-[3rem] p-4 xl:w-[45%] mr-[2rem] 2xl:w-[40%]">
-                            <h1 class="text-2xl font-bold">Lorem ipsum</h1>
-                            <p class="text-xl">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                            <h1 class="text-2xl font-bold lg:max-xl:text-xl">Lorem ipsum</h1>
+                            <p class="text-xl lg:max-xl:text-base">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
                         </div>
                     </div>
                 </div>
